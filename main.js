cd1a1c1e370cfb022f75f6380a0de2bc8ee345a3
lukeisluke <hello@lukeingle.com>
2013-11-09 14:36:14 +1100

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ showAlert: function (message, title) {
         alert(title ? (title + ": " + message) : message);
     }
 },
+
+renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+},
