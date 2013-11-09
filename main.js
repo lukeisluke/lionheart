79ae5a20da331e48da6e89b6fff2c5f9e1a1eaec
lukeisluke <hello@lukeingle.com>
2013-11-09 14:33:14 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+},
